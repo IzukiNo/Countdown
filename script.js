8fbc0b35cdaa414d56ac76954ed98b213bfc5d2e
IzukiNo <giabao1811@gmail.com>
2024-12-28 19:01:30 +0700

    update

---
+++ script.js
@@ -0,0 +1,34 @@
+function updateCountdown() {
+  const newYear = new Date("January 1, 2025 00:00:00").getTime();
+  const now = new Date().getTime();
+  const timeLeft = newYear - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+
+setInterval(updateCountdown, 1000);
+
+function now() {
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const formattedDate = today.toLocaleDateString("vi-VN", options);
+  document.getElementById("today").textContent = formattedDate;
+}
+
+document.addEventListener("DOMContentLoaded", now);
